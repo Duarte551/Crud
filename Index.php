48d5c368e76bd46b6a800e3edbf662aa81bb7526
Duarte <joaovictordfs551@gmail.com>
2022-03-22 17:02:46 -0300

    Botei fé

---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\db\Database ;
+
+//Chamada do Autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+//Chamada do header 
+require_once __DIR__ . '/Includes/Header.php';
+
+//Chamada do footer
+require_once __DIR__ . '/Includes/Footer.php';
+
