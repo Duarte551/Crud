da7fadc30da399ee68bac0a8085daa5795bee9e9
Duarte <joaovictordfs551@gmail.com>
2022-03-30 14:40:02 -0300

    Atualizando

--- app/db/Database.php
+++ app/db/Database.php
@@ -13,7 +13,7 @@ class Database
     
     //Constante que define o nome do banco de dados
 
-    const NAME = 'soufuturebd';
+    const NAME = 'soufuturedb';
 
     //Constante que define o nome de usuário
 
@@ -25,7 +25,7 @@ class Database
 
     //Atributo que vai receber a tabela 
 
-    private $tabela;
+    private $table;
 
     //Atributo que define a conexão com o banco de dados
 
@@ -37,28 +37,93 @@ class Database
     {  
         try {
             //Método pra chamada de atributo de forma pública
+
         $this->connection = new PDO('mysql:host=' . self::HOST . 'dbname=' . self::NAME, self::USER, self::PASS);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         //Direção para tela de errro 
+
         catch (PDOException $erro) {
             // Mensagem de erro 
+
             die ('Fatal Error' . $erro ->getmessage());
         }
     }
     
     //Metódo para executar o comando SQL 
 
-    public function execute ($query, $params = [])
+    public function execute($query, $params = [])
     {
         try{
             $statement = $this->connection ->prepare($query);
-            $statement ->execute($params);
+            $statement -> execute($params);
             return $statement;
         }
         catch(PDOException $error){
-            die ('Fatal Error' . $error ->getmessage());
+            die ('Fatal Error 2' . $error ->getmessage());
         }
+    }
+
+    //Metódo de criação de dados do banco  
+
+    public function insert($values)
+    {
+        //Passa os dados pra query
+
+        $fields = array_keys($values);
+        $binds = array_pad([], count ($fields), '?');
+
+        //Monta a query 
+
+        $query = 'INSERT INTO' . $this->table . '(' . implode(',', $fields) . ') VALUES ('. implode(',', $binds);
+
+        //Executa a query 
+
+        $this->execute($query, array_values($values));
+
+        //Retorna o último id inserido
+
+        return $this->connection->lastInsertId();
+    }
+
+    public function select($where = NULL, $order = NULL, $limit = NULL, $fields = '*')
+    {
+        //Preparar os dados da query    
+
+        $where = strlen($where) ? 'WHERE '. $where : '';
+        $order = strlen($order) ? ' ORDER BY '. $order : '';
+        $limit = strlen($limit) ? ' LIMIT '. $limit : '';
+
+        //Montando a query  
+
+        $query = 'SELECT' . $fields . ' FROM ' . $this->table . ' ' . $where . ' ' . $order . ' ' . $limit;
+
+        //Executa a query
+
+        $this->execute ($query);
+    }
+
+    public function update($where, $values)
+    {
+        //Dados da query sendo passados
+
+        $fields = array_keys($values);
+
+        //Montando a query 
+
+        $query = 'UPDATE' . $this->table . ' SET ' . implode('=?', $fields) . '=? WHERE' . $where ;
+    }
+
+    public function delete($where)
+    {
+        //Montar a query
+        $query = 'DELETE FROM ' . $this->table . 'WHERE' . $where; 
+
+        //Executa a query 
+        $this->execute ($query); 
+
+        //Retorna sucesso
+        return true;
     }
 
 }
