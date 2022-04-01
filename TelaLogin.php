7a2905de97de2077db0ffd512223d673cacfd89f
Duarte <joaovictordfs551@gmail.com>
2022-04-01 17:04:31 -0300

    Atualizando

---
+++ TelaLogin.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once __DIR__ . '/Index.php';
+
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+         <title>WebStack3</title>
+         <link rel="stylesheet" type="text/css" href="/Includes/Resources/login.css">
+
+    </head>
+
+<body id="corpo">
+
+
+    <div id="tela_login">
+      <h1>Login</h1>
+      <input type="text" placeholder="Nome">
+      <br> <br>
+      <input type="password" placeholder="Senha">
+      <br> <br>
+      <button>Enviar</button>
+
+    </div>
+</body>
+
+</html>
